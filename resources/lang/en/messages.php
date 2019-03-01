027e548eede095274046cce87e853c256ead6882
ChungTM1209 <minhchung.ctk17@gmail.com>
2019-03-01 08:45:30 +0700

    BC2_SS23_i18n

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chungtran
+ * Date: 01/03/2019
+ * Time: 08:36
+ */
+return [
+    'title' => 'Message was written by english.',
+];
---
+++ resources/lang/vi/messages.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chungtran
+ * Date: 01/03/2019
+ * Time: 08:37
+ */
+return [
+    'title' => 'Tin nhắn được viết bằng tiếng việt.',
+];
